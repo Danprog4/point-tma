89684e7293191fce6d1e0f81ecb29152db8d1e6f
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-07-25 13:55:48 +0500

    feat: integrate notifications and user balance display in Header component, enhancing user experience with real-time updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
 import { Bell } from "lucide-react";
+import { useTRPC } from "~/trpc/init/react";
 import { Coin } from "./Icons/Coin";
 import { Logo } from "./Icons/Logo";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const trpc = useTRPC();
+  const { data: notifications } = useQuery(trpc.main.getNotifications.queryOptions());
+  const { data: user } = useQuery(trpc.main.getUser.queryOptions());
+
+  const splitBalanceToK = () => {
+    if (user?.balance && user?.balance > 1000) {
+      return (user?.balance / 1000).toFixed(user?.balance % 1000 === 0 ? 0 : 1);
+    } else {
+      return user?.balance;
+    }
+  };
 
   return (
     <header className="fixed top-0 right-0 left-0 z-50 flex items-center justify-between bg-white p-4">
@@ -15,12 +28,21 @@ export const Header = () => {
       </div>
       <div className="flex items-center gap-2 rounded-full border border-gray-300 px-3 py-1">
         <Coin />
-        <span className="text-sm font-medium">0</span>
+        <span className="text-sm font-medium">{splitBalanceToK()}К</span>
       </div>
       <div className="flex w-[81px] items-center justify-end gap-4">
-        <button className="flex" onClick={() => navigate({ to: "/notif" })}>
-          <Bell className="h-5 w-5 text-gray-700" />
-        </button>
+        {notifications && notifications.length > 0 ? (
+          <button className="relative flex" onClick={() => navigate({ to: "/notif" })}>
+            <Bell className="h-5 w-5 text-gray-700" />
+            <div className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              {notifications.length}
+            </div>
+          </button>
+        ) : (
+          <button className="flex" onClick={() => navigate({ to: "/notif" })}>
+            <Bell className="h-5 w-5 text-gray-700" />
+          </button>
+        )}
       </div>
     </header>
   );

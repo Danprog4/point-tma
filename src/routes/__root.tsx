c9f23c9b161451f2adcb96a405267fc16bbc6d80
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-08-02 12:25:31 +0500

    feat: add eruda for tests

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -138,7 +138,7 @@ function RootDocument({ children }: { readonly children: React.ReactNode }) {
   const trpc = useTRPC();
 
   useEffect(() => {
-    if (isDev) {
+    if (isErudaEnabled) {
       import("eruda").then((eruda) => {
         eruda.default.init();
       });

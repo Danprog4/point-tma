2e546ae7a36388b0c8d11e1ba3bdc4671cbb53e4
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-09-02 13:32:43 +0500

    feat:L delete debugging eruda

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -157,7 +157,7 @@ function RootDocument({ children }: { readonly children: React.ReactNode }) {
   const trpc = useTRPC();
 
   useEffect(() => {
-    if (true) {
+    if (isDev) {
       import("eruda").then((eruda) => {
         eruda.default.init();
       });

be9d95cb21ecd65d2884b968f4cc7adbd40e7243
Danprog4 <danikpavlovski746@gmail.com>
2025-10-18 18:58:05 +0300

    fix: update Raleway font import to include italic styles and adjust weight range for better typography

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -59,7 +59,7 @@ export const Route = createRootRouteWithContext<{
       },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap",
       },
     ],
     scripts: [

93332ad2e81eac11cb7ac5afe6e9c86bdb14b622
Danprog4 <danikpavlovski746@gmail.com>
2025-12-12 11:25:07 +0300

    feat: debug mode eliminated

--- src/trpc/notifications.ts
+++ src/trpc/notifications.ts
@@ -7,7 +7,7 @@ import { createTRPCRouter, procedure } from "./init";
 export const notificationsRouter = createTRPCRouter({
   getNotifications: procedure.query(async ({ ctx }) => {
     const notifications = await db.query.notificationTable.findMany({
-      where: eq(notificationTable.fromUserId, ctx.userId),
+      where: eq(notificationTable.toUserId, ctx.userId),
       orderBy: [desc(notificationTable.createdAt)],
     });
 
